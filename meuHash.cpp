1bb8708d199206d8973ca922458e138880059df3
Matheus Oliveira <Matheus7op@gmail.com>
2018-02-08 08:13:22 -0300

    Update meuHash.cpp

--- meuHash.cpp
+++ meuHash.cpp
@@ -10,11 +10,10 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> P;
 
-#define MAXN 15007
+#define MAXN 15002
 
 struct DoubleHash {
 	ll powers1[MAXN], powers2[MAXN], values1[MAXN], values2[MAXN], prime1, prime2;
-	int strSize;
 
 	DoubleHash(const string &word, const ll base1 = 419, const ll base2 = 521, const ll prime1 = 82517999, const ll prime2 = 76824091) {
 		precalcPowers(powers1, base1, prime1);
@@ -23,7 +22,6 @@ struct DoubleHash {
 		precalcValues(values1, base1, prime1, powers1, word);
 		precalcValues(values2, base2, prime2, powers2, word);
 
-		this->strSize = ((int) word.size());
 		this->prime1 = prime1;
 		this->prime2 = prime2;
 	}
@@ -36,9 +34,10 @@ struct DoubleHash {
 		}
 	}
 
-	void precalcValues(ll *values, const ll base, const ll prime, ll *powers, const string &word) {
-		int aux = ((int) word.size()) - 1;
-		for(int i = ((int) word.size()); i >= 0; i--) {
+    void precalcValues(ll *values, const ll base, const ll prime, ll *powers, const string &word) {
+    	int tam = ((int) word.size());
+    	values[tam] = 0;
+		for(int i = tam - 1; i >= 0; i--) {
 			values[i] = ( word[i] + 307 ) + (values[i+1] * base);
 			values[i] %= prime;
 		}
@@ -47,13 +46,11 @@ struct DoubleHash {
 	P hashSubstr(int l, int r) {
 		P h;	
 
-		h.first = values1[l] - values1[r+1] * powers1[r - l + 1];
-		h.first %= this->prime1;
+		h.first = (values1[l] - values1[r+1] * powers1[r - l + 1]) % this->prime1;
 		h.first += this->prime1;
 		h.first %= this->prime1;
 
-		h.second = values2[l] - values2[r+1] * powers2[r - l + 1];
-		h.second %= this->prime2;
+		h.second = (values2[l] - values2[r+1] * powers2[r - l + 1]) % this->prime2;
 		h.second += this->prime2;
 		h.second %= this->prime2;
 

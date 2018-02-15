aed08a8a14db5d653edd65c0a9b24acce150b4db
Matheus Oliveira <Matheus7op@gmail.com>
2018-02-15 16:07:36 -0300

    Create KMP.cpp

---
+++ KMP.cpp
@@ -0,0 +1,52 @@
+/*
+ * Matheus Oliveira
+ * 15/02/2018
+ * KMP.cpp
+ * O(M+N)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 3000000
+int prefixVal[MAXN];
+
+void buildPrefixFunction(const string &word) {
+	prefixVal[0] = prefixVal[1] = 0;
+	int j;
+	for(int i = 2; i <= ((int) word.size()); i++) {
+		j = prefixVal[i-1];
+		while(true) {
+			if(word[i-1] == word[j]) {
+				prefixVal[i] = j+1;
+				break;
+			}
+
+			if(j == 0) {
+				prefixVal[i] = 0;
+				break;
+			}
+
+			j = prefixVal[j];
+		}
+	}
+}
+
+bool searchSubstr(const string &word, const string &text) {
+	int i, j;
+	i = j = 0;
+
+	while(true) {
+		if(j == ((int) word.size())) break;
+		if(word[j] == text[i]) {
+			i++;
+			j++;
+			if(i == ((int) text.size())) return true;
+		}
+
+		if(i > 0) i = prefixVal[i];
+		else j++;
+	}
+
+	return false;
+}

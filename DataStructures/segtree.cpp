d7804136639bfd85df1648fc7858b68447193cd4
Matheus Oliveira <matheus7op@gmail.com>
2019-02-13 17:00:33 -0300

    Organizing directories

--- DataStructures/segtree.cpp
+++ DataStructures/segtree.cpp
@@ -9,15 +9,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define EPS 1e-9
-#define INF 2e10
 #define MAXN 201110
-#define pb(a) push_back(a)
-#define mp(a, b) make_pair(a, b)
-#define debug(x) printf("%s: %d\n", #x, x);
-#define debugarr(x, ini, fim) printf("%s: ", #x); for(int i = ini; i <= fim; i++) printf("%d%c", x[i], i == fim ? '\n' : ' ');
 typedef long long ll;
-typedef pair<int, int> P;
 
 ll elements[MAXN], segtree[4*MAXN];
 

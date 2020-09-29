dea589ba2a4c51ae960169968cee57c3bb3b654a
Matheus Oliveira <matheus7op@gmail.com>
2020-09-29 07:13:59 -0300

    adding convex hull trick

---
+++ DataStructures/convexHullTrick.cpp
@@ -0,0 +1,40 @@
+/**
+ * Description: Container where you can add lines of the form kx+m, and query maximum values at points x.
+ *
+ * What if slopes are sorted in increasing order instead?
+ * Observe that negating the slope has the effect of mirroring lines about the Y-axis, so you can use one implementation for both.
+ *
+ * What if minimum is required instead of maximum?
+ * Observe that negating both slope and Y-intersect has the effect of mirroring about the X-axis.
+ */
+
+struct Line {
+	mutable ll k, m, p;
+	bool operator<(const Line& o) const { return k < o.k; }
+	bool operator<(ll x) const { return p < x; }
+};
+
+struct LineContainer : multiset<Line, less<>> {
+	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
+	static const ll inf = LLONG_MAX;
+	ll div(ll a, ll b) { // floored division
+		return a / b - ((a ^ b) < 0 && a % b); }
+	bool isect(iterator x, iterator y) {
+		if (y == end()) return x->p = inf, 0;
+		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+		else x->p = div(y->m - x->m, x->k - y->k);
+		return x->p >= y->p;
+	}
+	void add(ll k, ll m) {
+		auto z = insert({k, m, 0}), y = z++, x = y;
+		while (isect(y, z)) z = erase(z);
+		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+		while ((y = x) != begin() && (--x)->p >= y->p)
+			isect(x, erase(y));
+	}
+	ll query(ll x) {
+		assert(!empty());
+		auto l = *lower_bound(x);
+		return l.k * x + l.m;
+	}
+};
